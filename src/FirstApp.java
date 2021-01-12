c72691102e0bc5e283b1fc6674895be6b193286e
Kirill_Gigante <kdgigante@yandex.ru>
2021-01-12 16:35:42 +0300

    Completed homework version 2

--- src/FirstApp.java
+++ src/FirstApp.java
@@ -15,27 +15,22 @@ public class FirstApp {
         //Задание 3
         System.out.println("Значение выражения a * (b + (c / d)) равняется " + function(1, 2, 3, 4));
         //Задание 4
-        System.out.println("Сумма 2х чисел находится в пределах от 10 до 20? " + sum());
+        System.out.println("Сумма 2х чисел находится в пределах от 10 до 20? " + sum(1, 3));
         //Задание 5
-        sign();
+        sign(7);
         //Задание 6
-        System.out.println("Число отрицательное? " + signBolean());
+        System.out.println("Число отрицательное? " + signBolean(-1));
         //Задание 7
-        MyName();
+        MyName("Кирилл");
         //Задание 8
-        leapYear();
+        leapYear(2020);
     }
 
     static float function(float a, float b, float c, float d) {
         return a * (b + (c / d));
     }
 
-    static boolean sum() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число");
-        int g = scanner.nextInt();
-        System.out.println("Введите второе число");
-        int j = scanner.nextInt();
+    static boolean sum(int g, int j) {
         int o = g + j;
         if (o >= 10 && o <= 20) {
             return true;
@@ -44,10 +39,7 @@ public class FirstApp {
         }
     }
 
-    static void sign() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число");
-        int u = sc.nextInt();
+    static void sign(int u) {
         if (u >= 0) {
             System.out.println("Число " + u + " положительное");
         } else {
@@ -56,10 +48,7 @@ public class FirstApp {
 
     }
 
-    static boolean signBolean() {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Введите число");
-        int p = scn.nextInt();
+    static boolean signBolean(int p) {
         if (p < 0) {
             return true;
         } else {
@@ -67,17 +56,12 @@ public class FirstApp {
         }
     }
 
-    static void MyName() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имя");
-        String name = scanner.nextLine();
+    static void MyName(String name) {
         System.out.println("Привет, " + name + "!");
     }
 
-    static void leapYear() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите год");
-        int year = scanner.nextInt();
+    static void leapYear(int year) {
+
         if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
             System.out.println(year + " год является високосным");
 
